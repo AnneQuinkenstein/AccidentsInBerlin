076c2b48daf13f0244c209e4c9c6e60055e79848
kasch <karenina.schroeder@student@htw-berlin.de>
2024-06-14 15:08:54 +0200

    weiter mit random, stehen geblieben da wo alles immer abstürzt.. - also nicht fertig. alles in first_tests

---
+++ first_tests/knn_with_random_numbers.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+data_with_random_col = pd.read_csv('../data/GeneralDatensatz18-21ohneGeo.csv', sep=';')
+data_with_random_col['randNumCol'] = np.random.randint(0,1000, size=len(data_with_random_col))
+data_with_random_col['randNumCol2'] = np.random.randint(0,1000, size=len(data_with_random_col))
+data_with_random_col['randNumCol3'] = np.random.randint(0,1000, size=len(data_with_random_col))
+
+X = data_with_random_col.drop('UKATEGORIE', axis=1)
+y = data_with_random_col['UKATEGORIE']
+X = X.drop('LOR_ab_2021', axis = 1)
+# print(X.isnull().any())
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+knn = KNeighborsClassifier(n_neighbors=9)
+knn.fit(X_train, y_train)
+score = knn.score(X_test, y_test)
+print(score)
